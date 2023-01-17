f1ce535f6d82c774679cf8395607206b792699f6
Aleksei Viunik <vjunik88@gmail.com>
2023-01-17 22:21:31 +0300

    Graph was created. Data was dumped in pickle file

--- src/parse.py
+++ src/parse.py
@@ -1,11 +1,37 @@
 import json
+import os
+import pickle
+import sqlite3
 
 from os.path import join, abspath
 
+FILES_PATH = join('..', 'Data')
+JSON_FILE_NAME = 'scheme.json'
 
-JSON_PATH = join('.', 'Data', 'scheme.json')
-JSON_PATH = abspath(JSON_PATH)
-print(JSON_PATH)
+# Pickle file doesn't have the extension. 
+# You can name the file as you wish without it.
+# The same for sqlite3 file
+PICKLE_FILE_NAME = 'scheme.pickle'
+SQL_FILE_NAME = 'scheme.sqlite3'
+
+JSON_PATH = abspath(join(FILES_PATH, JSON_FILE_NAME))
+DATA_PATH = abspath(join(FILES_PATH, PICKLE_FILE_NAME))
+DB_PATH = abspath(join(FILES_PATH, SQL_FILE_NAME))
+
+
+def get_station_name(id_st):
+    '''There might be several stations with the same name (for example 'Belorusskaya').
+    But for our graph we need only one certain na,e with this name. '''
+    try:
+        st_text = stations[id_st]['name']
+    except KeyError:
+        st_text = f'<<< {id_st} >>>'
+    try:
+        line_text = lines[stations[id_st]['line_id']]['name']
+    except KeyError:
+        line_text = '??????'
+    result = st_text + ' (' + line_text + ')'
+    return result
 
 # Get dict from json as scheme. File closes automaticaly
 with open(JSON_PATH, 'rt', encoding='utf-8') as src:
@@ -59,5 +85,39 @@ for con in scheme['data']['connections']:
 
 # Graph's creation
 graph = {}
+for key in stations:
+    graph[key] = {'name': get_station_name(key)}
+for val in transitions.values():
+    id1, id2, ln12 = val['from_id'], val['to_id'], val['length']
+    pers, bi = val['perspective'], val['bi']
+    if (id1 in graph) and (id2 in graph) and (not pers) and bi:
+        graph[id1][id2] = ln12
+        graph[id2][id1] = ln12
+for val in connections.values():
+    id1, id2, ln12 = val['from_id'], val['to_id'], val['length']
+    pers, bi = val['perspective'], val['bi']
+    if (id1 in graph) and (id2 in graph) and (not pers) and bi:
+        graph[id1][id2] = ln12
+        graph[id2][id1] = ln12    
+
+# Save (pack) our graph's dict and others dict to pickle file.
+# Order is important! First in - first out.
+with open(DATA_PATH, 'wb') as destination:
+    pickle.dump(graph, destination)
+    pickle.dump(lines, destination)
+    pickle.dump(stations, destination)
+print(f'All data are saved in "{DATA_PATH}" file')
+
+
+# If DB_file is created, we remove it in case it filled wrong.
+try:
+    os.remove(DB_PATH)
+except FileNotFoundError:
+    pass
+
+# Connect Database
+con = sqlite3.connect(DB_PATH)
+cur = con.cursor()
+
 
 print('END')
